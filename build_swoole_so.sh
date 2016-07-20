012e9440b2f42b5dbf20990f8d1e6ff53ddecbc5
WANJO CHAN <wanjochan@users.noreply.github.com>
2016-07-21 00:27:22 +0800

    Update build_swoole_so.sh

--- build_swoole_so.sh
+++ build_swoole_so.sh
@@ -5,6 +5,8 @@
 apk update
 apk upgrade
 apk add git autoconf build-base linux-headers libaio-dev php7-dev
+ln -s /usr/bin/php-config7 /usr/bin/php-config
+ln -s /usr/bin/phpize7 /usr/bin/phpize
 cd /root/
 git clone https://github.com/swoole/swoole-src.git
 cd swoole-src/
